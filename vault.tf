fb04847b3060444c7f55cf3c30ea6c30b95610cd
Naman Singh <120774187+namansingh1314@users.noreply.github.com>
2025-04-23 14:21:49 +0530

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "nrg" {
 }
 
 # Key Vault resource definition
-resource "azurerm_key_vault" "nrg" {
+resource "azurerm_key_vault" "nrg" { 
   name                        = "uneekkeevault"
   location                    = azurerm_resource_group.nrg.location
   resource_group_name         = azurerm_resource_group.nrg.name
